cae4b234fce5e39f135a10c60285ee175df9c1b3
Ali Can Öztürk <alican@caunos.com>
2023-08-01 20:50:55 +0300

    Zipkin introduction and gateway caching

--- fx-rate-service/src/main/java/com/xchangeapp/fxrateservice/service/FxRateService.java
+++ fx-rate-service/src/main/java/com/xchangeapp/fxrateservice/service/FxRateService.java
@@ -4,10 +4,8 @@ import com.google.gson.JsonObject;
 import com.xchangeapp.fxrateservice.client.ExchangeRateApiFeignClient;
 import com.xchangeapp.fxrateservice.data.Currency;
 import com.xchangeapp.fxrateservice.repository.FxRateRepository;
-import com.xchangeapp.fxrateservice.util.Constant;
 import com.xchangeapp.fxrateservice.util.FxRateUtil;
 import lombok.AllArgsConstructor;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,7 +16,6 @@ public class FxRateService {
     
     private final FxRateRepository fxRateRepository;
     
-    @Cacheable(value = Constant.CACHE_FX_RATES, key = "#currency.name()")
     public JsonObject getLatestRates(Currency currency) {
         final JsonObject apiResponse = exchangeRateApiFeignClient.getLatestRates(currency);
         final JsonObject latestRates = FxRateUtil.removeUnnecessaryFields(apiResponse);
--- fx-rate-service/src/main/java/com/xchangeapp/fxrateservice/util/Constant.java
+++ fx-rate-service/src/main/java/com/xchangeapp/fxrateservice/util/Constant.java
@@ -4,8 +4,6 @@ import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class Constant {
-    
-    public final String CACHE_FX_RATES = "fx-rates";
 
     public final String INDEX_FX_RATES = "fx-rates";
     
