7119d7e34cd5f570b8fe8a2fdfefcda2b1608675
Ali Can Öztürk <alican@caunos.com>
2023-08-18 19:10:13 +0300

    Logging filter update for gateway and zipkin config

--- gateway-service/src/main/java/com/xchangeapp/gatewayservice/filter/LoggingFilter.java
+++ gateway-service/src/main/java/com/xchangeapp/gatewayservice/filter/LoggingFilter.java
@@ -3,18 +3,43 @@ package com.xchangeapp.gatewayservice.filter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.cloud.gateway.route.Route;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
+import java.net.URI;
+import java.util.Collections;
+import java.util.Set;
+
+import static org.springframework.cloud.gateway.support.ServerWebExchangeUtils.*;
+
 @Component
 @Slf4j
 public class LoggingFilter implements GlobalFilter {
-    
+
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        log.info("Request received: {}", exchange.getRequest().getPath());
+        final Set<URI> uriList = exchange.getAttributeOrDefault(
+                GATEWAY_ORIGINAL_REQUEST_URL_ATTR,
+                Collections.emptySet()
+        );
+
+        final String originalUri = uriList.isEmpty()
+                ? "Unknown"
+                : uriList.iterator().next().toString();
+
+        final Route route = exchange.getAttribute(GATEWAY_ROUTE_ATTR);
+        final URI routeUri = exchange.getAttribute(GATEWAY_REQUEST_URL_ATTR);
+
+        log.info(
+                "Request received: {}. Forwarded to {} with id {}",
+                originalUri,
+                routeUri,
+                route.getId()
+        );
+
         return chain.filter(exchange);
     }
-    
+
 }
